42ce397ef69471781816ad6dabc0be062250dd52
Roger Hirooka <roger.hirooka@skipthedishes.ca>
2020-11-15 18:44:00 -0600

    added robostop file just in case

---
+++ robostop.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+'''
+robotest.py - Test the features of BreezyCreate2
+
+This code is part of BreezyCreate2
+
+The MIT License
+
+Copyright (c) 2016 Simon D. Levy
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+'''
+
+from breezycreate2 import Robot
+import time
+
+# Create a Create2. This will automatically try to connect to your robot over serial
+bot = Robot()
+
+# Tell the Create2 to turn right slowly
+bot.setTurnSpeed(0)
+bot.setForwardSpeed(0)
+
+# Wait a second
+time.sleep(1)
+
+# Close the connection
+bot.close()
